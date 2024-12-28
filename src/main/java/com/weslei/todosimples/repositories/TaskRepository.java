66bba9d6dcd19a8bf1b4badb4a545cca7caeb598
WesleiKhan <wesleikhan@gmail.com>
2024-12-28 10:52:08 -0300

    Create Repository Task

---
+++ src/main/java/com/weslei/todosimples/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.weslei.todosimples.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.weslei.todosimples.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_id(long id);
+    
+} 
