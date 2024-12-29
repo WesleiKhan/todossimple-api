8d05a998060ca99d4f82dcdbde7e41e0e3beaae7
WesleiKhan <wesleikhan@gmail.com>
2024-12-29 10:24:57 -0300

    Create Config Cors Headrs

---
+++ src/main/java/com/weslei/todosimples/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.weslei.todosimples.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
