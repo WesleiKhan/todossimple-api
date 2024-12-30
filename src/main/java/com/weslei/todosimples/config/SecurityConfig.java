60b35a7b2cf4b12f3c78a296417300ac1856bce9
WesleiKhan <wesleikhan@gmail.com>
2024-12-30 10:42:20 -0300

    add dependecies and create SecurityConfig

--- src/main/java/com/weslei/todosimples/config/SecurityConfig.java
+++ src/main/java/com/weslei/todosimples/config/SecurityConfig.java
@@ -1,19 +1,70 @@
 package com.weslei.todosimples.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    private static final String[] PUBLIC_MATCHERS = {
+        "/"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+        "/user",
+        "/login"
+    };
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configura CORS
             .csrf(csrf -> csrf.disable()) // Desabilita CSRF
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Permite todas as rotas
+            .authorizeHttpRequests(auth -> {
+                // Permite requisições públicas
+                auth.requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll();
+                auth.requestMatchers(PUBLIC_MATCHERS).permitAll();
+                auth.anyRequest().authenticated();
+            })
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Define a política de sessão
+
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+
+        configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;           
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
 }
