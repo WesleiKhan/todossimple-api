35ec6dad7b636551d16624975cb6db2d996255c7
WesleiKhan <wesleikhan@gmail.com>
2024-12-30 12:05:43 -0300

    add password encode and Create User Profiles

--- src/main/java/com/weslei/todosimples/models/User.java
+++ src/main/java/com/weslei/todosimples/models/User.java
@@ -1,17 +1,22 @@
 package com.weslei.todosimples.models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.weslei.todosimples.models.enums.ProfileEnum;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,10 +25,20 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @Entity
 @Table(name= User.Table_Name)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
 
     public interface CreateUser {}
@@ -43,102 +58,30 @@ public class User {
     private String username;
 
     @JsonProperty(access = Access.WRITE_ONLY)
-    @Column(name = "password", length = 50, nullable = false)
+    @Column(name = "password", length = 100, nullable = false)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class}) 
-    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 50)
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 100)
     private String password;
 
 
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = Access.WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
 
 
-    public User() {}
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @CollectionTable(name = "user_profile")
+    @Column(name = "profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
 
-    public User(Long id, String username, String password) {
-
-        this.id = id;
-        this.username = username;
-        this.password = password;
-
+    public Set<ProfileEnum> getProfiles() {
+        return this.profiles.stream().map(x -> ProfileEnum.toEnum(x)).collect(Collectors.toSet());
     }
 
-    public long getId() {
-
-        return this.id;
-    }
-
-    public void setId(Long id) {
-
-        this.id = id;
-    }
-
-    public String getUsername() {
-
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-
-        this.username = username;
-    }
-
-    public String getPassword() {
-
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-
-        this.password = password;
-    }
-
-    public List<Task> getTask() {
-
-        return this.tasks;
-    }
-
-    public void setTask(List<Task> tasks) {
-
-        this.tasks = tasks;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-
-        if (obj == this) {
-
-            return true;
-        }
-
-        if (obj == null) {
-
-            return false;
-        }
-
-        if (!(obj instanceof User)) {
-
-            return false;
-        }
-
-        User other = (User) obj;
-
-        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) && Objects.equals(this.password, other.password);
-        
-    }
-
-
-    @Override
-    public int hashCode() {
-
-        final int prime = 31;
-        int result = 1;
-        
-        result = prime * result + Long.hashCode(this.id);
-
-        return result;
+    public void addProfile(ProfileEnum profileEnum) {
+        this.profiles.add(profileEnum.getCode());
     }
     
 }
--- src/main/java/com/weslei/todosimples/services/UserServices.java
+++ src/main/java/com/weslei/todosimples/services/UserServices.java
@@ -1,19 +1,25 @@
 package com.weslei.todosimples.services;
 
 import java.util.Optional;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.weslei.todosimples.models.User;
+import com.weslei.todosimples.models.enums.ProfileEnum;
 import com.weslei.todosimples.repositories.UserRepository;
 import com.weslei.todosimples.services.exceptions.DataBindingViolationException;
 import com.weslei.todosimples.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServices {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -33,6 +39,10 @@ public class UserServices {
 
         obj.setId(null);
 
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
+
         obj = this.userRepository.save(obj);
 
         return obj;
@@ -44,6 +54,8 @@ public class UserServices {
         User newObj = findById(obj.getId());
 
         newObj.setPassword(obj.getPassword());
+
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
 
         return this.userRepository.save(newObj);
     }
