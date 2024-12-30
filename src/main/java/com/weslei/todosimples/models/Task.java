6c6a9880e0d471ca8bec788c09618a6d085c3b73
WesleiKhan <wesleikhan@gmail.com>
2024-12-30 12:08:34 -0300

    Refatoretion Model Task

--- src/main/java/com/weslei/todosimples/models/Task.java
+++ src/main/java/com/weslei/todosimples/models/Task.java
@@ -1,7 +1,5 @@
 package com.weslei.todosimples.models;
 
-import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
@@ -16,10 +14,18 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @Entity
 @Table(name = Task.Table_Name)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Task {
 
     public static final String Table_Name = "task";
@@ -38,82 +44,5 @@ public class Task {
     @Size(min = 1, max = 300)
     @Column(name = "description", nullable = false, length = 300)
     private String description;
-
-
-    public Task() {}
-
-    public Task(Long id, User user, String description) {
-
-        this.id = id;
-        this.user = user;
-        this.description = description;
-
-    }
-
-    public long getId() {
-
-        return this.id;
-    }
-
-    public void setId(Long id) {
-
-        this.id = id;
-    }
-
-    public User getUser() {
-
-        return this.user;
-    }
-
-    public void setUser(User user) {
-
-        this.user = user;
-    }
-
-    public String getDescription() {
-
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-
-        this.description = description;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-
-        if (obj == this) {
-
-            return true;
-        }
-
-        if (obj == null) {
-
-            return false;
-        }
-
-        if (!(obj instanceof Task)) {
-
-            return false;
-        }
-
-        Task othertask = (Task) obj;
-
-        return Objects.equals(this.id, othertask.id) && Objects.equals(this.user, othertask.user) && Objects.equals(this.description, othertask.description);
-    }
-
-
-    @Override
-    public int hashCode() {
-
-        final int prime = 31;
-        int result = 1;
-
-        result = prime * result + Long.hashCode(this.id);
-
-        return result;
-    }
-    
+   
 }
